624396cf43f96a70ed83b8806b6044d144a3ab41
spuddobuddo <tristanrojo7@gmail.com>
2023-02-23 10:13:30 -0800

    2/23/2023 update autoclicker

--- src/App.js
+++ src/App.js
@@ -51,8 +51,20 @@ export default function App() {
   const [disableTen, setDisableTen] = useState(false);
   const [disableFifty, setDisableFifty] = useState(false);
   const [disableHundred, setDisableHundred] = useState(false);
+  const [disableFivehundred, setDisableFivehundred] = useState(false);
 
-  const [trans, setTrans] = useState("red");
+  const [autoClicker, setAutoClicker] = useState(1);
+
+  const [transMulti, setTransMulti] = useState("red");
+  const [transAuto, setTransAuto] = useState("blue");
+
+  function autoClickerIncrease(){
+    setCounter((count) => count + autoClicker)
+  }
+
+  function getAutoClickNum(){
+    return autoClicker;
+  }
 
   return (
     <div>
@@ -75,6 +87,7 @@ export default function App() {
         <div className="App">
           <div>Total Clicks: <span style={{color: '#1CDA16'}}>{getTotalCount()}</span></div>
           <div>Multiplier: <span style={{color: 'red'}}>{getUpIncrement()}</span></div>
+          <div>Auto Clicker: <span style={{color: 'blue'}}>{getAutoClickNum()}</span></div>
           <br></br>
         </div>
       </div>
@@ -107,11 +120,25 @@ export default function App() {
               decrease(100)
               okayPlay()
               setUpIncrement(2);
-              setTrans("transparent")
+              setTransMulti("transparent")
             }}}>
            <div>100</div>
-           <div className="trans" style={{color: trans, fontSize: 50}}>2X</div>
+           <div className="transMulti" style={{color: transMulti, fontSize: 50}}>2X</div>
           </button>
+
+          <button className="fivehundred_btn" disabled={disableFivehundred} onClick={() => {
+            if (getCount() >= 5)
+            {
+              setDisableFivehundred(true)
+              decrease(500)
+              okayPlay()
+              setTransAuto("transparent")
+              setInterval(() => {autoClickerIncrease()}, 1000)
+            }}}>
+           <div>500</div>
+           <div className="transAuto" style={{color: transAuto, fontSize: 50}}>Auto</div>
+          </button>
+
         </div>
       </div>
     </div>
